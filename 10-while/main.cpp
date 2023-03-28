8b76721f87f1760358780165551349a3830558a6
Daniel <dgaiteiro@gmail.com>
2023-03-28 22:06:42 +0200

    10 bucle while. done

--- 10-while/main.cpp
+++ 10-while/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     int valor = 0;
-
+    //estructura de repetición (bucle) while
     while (valor < 10) {
         qDebug() << valor;
         valor = valor + 1;
